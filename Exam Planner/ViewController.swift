a4f9fd204df9ad89a639740cc3027b0f9797de8c
misko <misko@miskos-imac.local>
2019-01-06 12:42:52 +0100

    Initial Commit

---
+++ Exam Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam Planner
+//
+//  Created by misko on 06/01/2019.
+//  Copyright © 2019 FMFI UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
